9d6d79010dd8a3ae41e3f5319669cd0d9577e994
QuangBinhDinh <g4.terminator@gmail.com>
2023-09-28 17:03:06 +0700

    fix lib (build IOS done)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import Router from './navigation';
+LogBox.ignoreAllLogs(); //Ignore all log notifications
+
 const App = () => {
     return <Router></Router>
 };
 
-export default App
+export default App;
